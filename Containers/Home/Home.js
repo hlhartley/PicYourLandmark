7ca177c09d6a302f01d731fb4b26c90ca859287a
Matthew Foxwell <foxwellmw@gmail.com>
2019-04-02 16:12:31 -0600

    Add Home Component, add MapView with dummy data

--- Containers/Home/Home.js
+++ Containers/Home/Home.js
@@ -1,80 +1,79 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps'; 
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 
 export default class Home extends Component {
   constructor() {
     super()
-
+    this.state = {
+      currentLocation: {
+        currentLat: 37.78825,
+        currentLon: -122.4324,
+        currentLatDelta: 0.015,
+        currentLonDelta: 0.0121
+      },
+      locations: [
+        {
+          id: 1,
+          lat: 37.78825,
+          lon: -122.4324,
+          visited: false
+        },
+        {
+          id: 2,
+          lat: 37.78828,
+          lon: -122.4334,
+          visited: true
+        }
+      ]
+    }
   }
 
 
-
-  // componentDidMount = () => {
-  //   // this.fetchLocations()
-  // }
-
   render() {
-
-
+    const { currentLat, currentLon, currentLatDelta, currentLonDelta } = this.state.currentLocation;
+    const { locations } = this.state;
     return (
       <View style={styles.container}>
-
         <MapView
-          provider={"google"} // remove if not using Google Maps
+          provider={"google"}
           style={styles.map}
           region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
+            latitude: currentLat,
+            longitude: currentLon,
+            latitudeDelta: currentLatDelta,
+            longitudeDelta: currentLonDelta,
           }}
           showsUserLocation={true}
         >
-          <Marker
-            coordinate={{
-              latitude: 37.78825,
-              longitude: -122.4324
-            }}
-            pinColor='#FFF000'
-            description={'hello'}
-          />
-          <Marker
-            coordinate={{
-              latitude: 37.78827,
-              longitude: -122.4330
-            }}
-            pinColor={'#000FFF'}
-          />
+          {
+            locations.map(location => {
+              const pinColor = location.visited ? '#FFF000' : '#000FFF';
+              return <Marker
+                key={location.id}
+                coordinate={{
+                  latitude: location.lat,
+                  longitude: location.lon
+                }}
+                pinColor={pinColor}
+              />
+            })
+          }
         </MapView>
       </View>
     );
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
 const styles = StyleSheet.create({
   container: {
-  //   position: 'absolute',
-  // left: 0,
-  //   right: 0,
-  //     top: 0,
-  //       bottom: 0,
-    height: 400,
-    width: 90,
-    // justifyContent: 'flex-end',
-    // alignItems: 'center',
+    left: '5%',
+    height: '60%',
+    width: '90%',
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    width: '100%',
+    height: '100%'
   },
 });
