41499d1184b6af6d058af2aaa0b5a870676bfc7d
Heather Hartley <hlhartley@gmail.com>
2019-04-06 17:45:36 -0600

    Removes unpassing test files

--- Containers/Footer/Footer.test.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
-import Footer from './Footer';
--- Containers/Home/Home.test.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
-import Home from './Home';
-
