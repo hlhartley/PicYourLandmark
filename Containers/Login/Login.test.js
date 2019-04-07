22e062b801fd21fa001b995f0a91c27e2d7eae46
Matthew Foxwell <foxwellmw@gmail.com>
2019-04-06 18:49:30 -0600

    Add Login test file and snapshot test

---
+++ Containers/Login/Login.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Login } from './Login';
+import renderer from 'react-test-renderer';
+
+describe('Login', () => {
+  test('renders correctly', () => {
+    const tree = renderer.create(<Login />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
