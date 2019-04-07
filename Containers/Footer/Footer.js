bded8597f517e4234def248c4159b5be70ec874a
Matthew Foxwell <foxwellmw@gmail.com>
2019-04-07 14:26:49 -0600

    Update Footer styling

--- Containers/Footer/Footer.js
+++ Containers/Footer/Footer.js
@@ -5,22 +5,24 @@ import { Icon } from 'react-native-elements';
 export class Footer extends Component {
 
   render() {
+    const {changeCurrentPage, currentPage} = this.props;
+    const containerHeight = currentPage === 'Camera' ? '15%' : '8%';
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, {height: containerHeight}]}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={{ backgroundColor: '#e9e9e9' }} onPress={() => this.props.changeCurrentPage('User profile')}>
+          <TouchableOpacity onPress={() => changeCurrentPage('User profile')}>
             <Icon color="#3c4859" name="user" type="font-awesome" size={30} />
           </TouchableOpacity>
-          <TouchableOpacity style={{ backgroundColor: '#e9e9e9' }} onPress={() => this.props.changeCurrentPage('Home')} >
+          <TouchableOpacity onPress={() => changeCurrentPage('Home')} >
             <Icon color="#3c4859" name="map-marker" type="font-awesome" size={30} />
           </TouchableOpacity>
-          <TouchableOpacity style={{ backgroundColor: '#e9e9e9' }} onPress={() => this.props.changeCurrentPage('Camera')}>
+          <TouchableOpacity onPress={() => changeCurrentPage('Camera')}>
             <Icon color="#3c4859" name="camera" type="font-awesome" size={30} />
           </TouchableOpacity>
-          <TouchableOpacity style={{ backgroundColor: '#e9e9e9' }} onPress={() => this.props.changeCurrentPage('Collected landmarks')}>
+          <TouchableOpacity onPress={() => changeCurrentPage('Collected landmarks')}>
             <Icon color="#3c4859" name="image" type="font-awesome" size={30} />
           </TouchableOpacity>
-          <TouchableOpacity style={{ backgroundColor: '#e9e9e9' }} onPress={() => this.props.changeCurrentPage('Login')}>
+          <TouchableOpacity onPress={() => changeCurrentPage('Login')}>
             <Icon color="#3c4859" name="sign-in" type="font-awesome" size={30} />
           </TouchableOpacity>
         </View>
@@ -33,11 +35,8 @@ const styles = StyleSheet.create({
   container: {
     position: 'absolute',
     bottom: 0,
-    height: '8%',
     width: '100%',
     backgroundColor: '#e9e9e9',
-    // borderTopColor: '#3c4859',
-    // borderTopWidth: 1,
   },
   buttonContainer: {
     flex: 1,
@@ -46,6 +45,5 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around'
   }
 });
-
 
 export default Footer;
