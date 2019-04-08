c11d4f273725606221b50e6e8d6dd036f3cb737c
Heather Hartley <hlhartley@gmail.com>
2019-04-08 16:40:02 -0600

    Puts dummy data in visitedLocations and maps over locations in CollectedLandmarksContainer rather than Landmark component

--- Containers/Landmark/Landmark.js
+++ Containers/Landmark/Landmark.js
@@ -5,46 +5,18 @@ import { Icon } from 'react-native-elements';
 export class Landmark extends Component {
   constructor() {
     super()
-    this.state = {
-      collectedLandmarks: [
-        {
-          id: 1,
-          name: 'Coors Field',
-          pointVal: 10,
-          visited: '4/1/2019'
-        },
-        {
-          id: 2,
-          name: 'Statue of Liberty',
-          pointVal: 10,
-          visited: '3/12/2019'
-        },
-        {
-          id: 3,
-          name: 'Grand Canyon',
-          pointVal: 10,
-          visited: '12/9/2018'
-        }
-      ]
-    }
   }
   render() {
     return (
       <View style={styles.container}>
-        {
-          this.state.collectedLandmarks.map((landmark => {
-            return (
-              <View key={landmark.id} style={styles.landmarkContainer}>
-                <Image source={require('../../assets/statueofliberty.jpg')} style={{ width: 300, height: 200, bottom: 15 }} />
-                <Text style={{ fontWeight: 'bold' }}>{landmark.name.toUpperCase()} ({landmark.visited})</Text>
-                <View style={{ display: 'flex' }}>
-                  <Icon color="#f44336" name="diamond" type="font-awesome" size={20} left={'-45%'} top={3} />
-                  <Text style={{ top: -15, left: 30 }}>{landmark.pointVal} gems</Text>
-                </View>
-              </View>
-            )
-          }))
-        }
+        <View style={styles.landmarkContainer}>
+          <Image source={require('../../assets/statueofliberty.jpg')} style={{ width: 300, height: 200, bottom: 15 }} />
+          <Text style={{ fontWeight: 'bold' }}>{this.props.landmark.name.toUpperCase()}</Text>
+          <View style={{ display: 'flex' }}>
+            <Icon color="#f44336" name="diamond" type="font-awesome" size={20} left={'-45%'} top={3} />
+            <Text style={{ top: -15, left: 30 }}>10 gems</Text>
+          </View>
+        </View>
       </View>
     )
   }
@@ -56,12 +28,10 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     left: '5%',
     width: '92%',
-    padding: 8,
-    flex: 1
+    padding: 8
   },
   landmarkContainer: {
-    padding: 10,
-    top: -200,
+    top: '-25%'
   }
 });
 
