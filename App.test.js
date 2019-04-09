ef82aaf6f2a48149a7f31f4e2fc8ef761ec9b092
Heather Hartley <hlhartley@gmail.com>
2019-04-08 20:51:22 -0600

    Adds method testing to App tests

--- App.test.js
+++ App.test.js
@@ -47,4 +47,61 @@ describe('App', () => {
     });
   });
 
+  describe('takeProfilePic method', () => {
+    it('should change state to what was passed as an argument', () => {
+      wrapper.setState({ takingProfilePic: false });
+      wrapper.instance().takeProfilePic()
+      expect(wrapper.state('takingProfilePic')).toEqual(true);
+    });
+  });
+
+  describe('savePicture method', () => {
+    it('should change state to what was passed as an argument if takingProfilePic is true', () => {
+      wrapper.setState({ takingProfilePic: true });
+      wrapper.instance().savePicture('newPic')
+      expect(wrapper.state('profilePic')).toEqual('newPic');
+      expect(wrapper.state('cameraLoading')).toEqual(false);
+      expect(wrapper.state('currentPage')).toEqual('User profile');
+      expect(wrapper.state('takingProfilePic')).toEqual(false)
+    });
+
+    it.skip('should call addLocationPhoto if takingProfilePic is false', () => {
+      wrapper.setState({ takingProfilePic: false, currentPhotoLocation: ''});
+      wrapper.instance().savePicture('newPic')
+      wrapper.instance().addLocationPhoto = jest.fn()
+      expect(wrapper.instance().addLocationPhoto).toHaveBeenCalled()
+    });
+  });
+
+  describe('takeLocationPhoto method', () => {
+    it('should change state to what was passed as an argument', () => {
+      const mockCurrentPhotoLocation = {
+        name: 'selectedName',
+        description: 'selectedDescription',
+        lat: 'selectedLatitude',
+        lon: 'selectedLongitude',
+        landmark_id: 'selectedID',
+        photo_url: ''
+      }
+      wrapper.instance().takeLocationPhoto('selectedName', 'selectedDescription', 'selectedLatitude', 'selectedLongitude', 'selectedID')
+      expect(wrapper.state('currentPhotoLocation')).toEqual(mockCurrentPhotoLocation);
+      expect(wrapper.state('currentPage')).toEqual('Camera')
+    });
+  });
+
+  describe('setCameraLoading method', () => {
+    it('should change state to what was passed as an argument', () => {
+      wrapper.setState({ cameraLoading: false })
+      wrapper.instance().setCameraLoading()
+      expect(wrapper.state('cameraLoading')).toEqual(true);
+    });
+  });
+
+  describe('setUserLoginId method', () => {
+    it('should change state to what was passed as an argument', () => {
+      wrapper.setState({ currentUserId: 1 })
+      wrapper.instance().setUserLoginId(2)
+      expect(wrapper.state('currentUserId')).toEqual(2);
+    });
+  });
 });
