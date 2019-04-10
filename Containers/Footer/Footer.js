d9736a401661c8137d62d64392e4b8ab9982a52c
Heather Hartley <hlhartley@gmail.com>
2019-04-10 14:03:27 -0600

    Updates footer tests

--- Containers/Footer/Footer.js
+++ Containers/Footer/Footer.js
@@ -13,7 +13,7 @@ export class Footer extends Component {
             <Icon color={this.props.currentPage === 'User profile' ? "#0b80dc" : "#3c4859"} name="user" type="font-awesome" size={30} />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => changeCurrentPage('Tutorial')}>
-            <Icon color={this.props.currentPage === 'Camera' ? "#0b80dc" : "#3c4859"} name="book" type="font-awesome" size={30} />
+            <Icon color={this.props.currentPage === 'Tutorial' ? "#0b80dc" : "#3c4859"} name="book" type="font-awesome" size={30} />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => changeCurrentPage('Home')} >
             <Icon color={this.props.currentPage === 'Home' ? "#0b80dc" : "#3c4859"} name="map-marker" type="font-awesome" size={30}/>
